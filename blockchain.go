b02154162eaf28fd30b329e2bec85b2424d8d486
Friedrich Doku <friedrichdoku@gmail.com>
2018-02-19 15:49:59 -0600

    Minior Change

--- blockchain.go
+++ blockchain.go
@@ -1,8 +1,19 @@
 package main
 
+import (
+	"github.com/boltdb/bolt"
+	"fmt"
+)
+
+
 type Blockchain struct {
-	blocks []*Block
+	tip []byte
+	db *bolt.DB
 }
+
+
+const dbFile = "demschain.db"
+const blocksBucket = "blocks"
 
 func (bc *Blockchain) AddBlock(data string) {
 	prevBlock := bc.blocks[len(bc.blocks)-1]
@@ -15,5 +26,23 @@ func NewGenesisBlock() *Block {
 }
 
 func NewBlockchain() *Blockchain {
-	return &Blockchain{[]*Block{NewGenesisBlock()}}
+	var tip []byte
+	db, err := bolt.Open(dbFile, 0600, nil)
+
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+
+		if b == nil {
+			genesis := NewGenesisBlock()
+			b, err := tx.CreateBucket([]byte(blocksBucket))
+			err = b.Put(genesis.Hash, genesis.Serialize())
+			err = b.Put([]byte("1"), genesis.Hash)
+			tip = genesis.Hash
+		} else {
+			tip = b.Get([]byte("1"))
+		}
+		return nil
+	})
+	bc := Blockchain{tip, db}
+	return &bc
 }
