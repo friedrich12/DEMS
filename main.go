8f93e59d976b0c75d41ff868f4f5f866f348ed23
Friedrich Doku <friedrichdoku@gmail.com>
2018-02-19 20:29:31 -0600

    Quick change

--- main.go
+++ main.go
@@ -8,16 +8,7 @@ import (
 func main() {
 	bc := NewBlockchain()
 
-	bc.AddBlock("Send 1 DMS to Alice")
-	bc.AddBlock("Send 2 more BTC to John")
-
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Println()
-		pow := NewProofOfWork(block)
-		fmt.Printf("Pow: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
+	defer bc.db.Close()
+	cli := CLI{bc}
+	cli.Run()
 }
